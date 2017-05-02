074ead13e6e347574775ecc4fd14352326782f0e
Yinyi Chen <yinyic@usc.edu>
2017-05-02 10:45:41 -0400

    change to upper case

--- backend/Simulator/message.hpp
+++ backend/Simulator/message.hpp
@@ -2,12 +2,12 @@
 #define MESSAGE_HPP
 
 
-enum CacheMessage {CACHE_READ, CACHE_READ_REPLY, 
+enum MessageType {CACHE_READ, CACHE_READ_REPLY, 
 					CACHE_UPDATE, CACHE_UPDATE_ACK,
 	              	CACHE_INVALIDATE, CACHE_INVALIDATE_ACK,
-	              	CACHE_FETCH, CACHE_FETCH_ACK
+	              	CACHE_FETCH, CACHE_FETCH_ACK,
 	              	CACHE_EVICTION_ALERT};
-struct message{
+struct Message{
     
     /* the ID of the sender */
 	int sourceID;
@@ -15,7 +15,6 @@ struct message{
 	MessageType msgType;
 	/* latency of communication */
 	uint latency;
-
 
 };
 
