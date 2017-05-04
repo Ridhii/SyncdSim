7cb4f7ee6b8173924e437a65dac65cc79167f667
ridhii <rsurana@andrew.cmu.edu>
2017-05-04 14:59:02 -0400

    commit for merge

--- backend/Simulator/directory.cpp
+++ backend/Simulator/directory.cpp
@@ -1,20 +1,71 @@
-#include "directory.cpp"
+#include "common.hpp"
+#include "context.hpp"
+#include "assert.h"
+#include <map>
 
 
 Directory::Direcotry(Context* context) {
+	
 	myContext = context;
+	numContexts = myContext->getNumContexts();
 	
 }
 
 Directory::~Direcotry() {}
 
 DirectoryEntryStatus Directory::lookUpEntry(uint64_t addr) {
+	if(directory.find(addr) == directory.end()){
+		return DirectoryEntryStatus.UNCACHED;
+	}
+	else{
+		return directory.find(addr).status;
+	}
 
 }
 
 
-void Directory::addEntry(unint64 addr, DirectoryEntryStatus status) {
+void Directory::addEntry(unint64 addr, DirectoryEntryStatus status, int procID) {
+	
+	if(directory.find(addr) == directory.end()){
+		DirectoryEntry* dirEntry = new DirectoryEntry;
+		dirEntry->processorMask.reserve(numContexts);
+		for(int i = 0; i < numContexts; i++){
+			if(i == procID){
+				dirEntry->processorMask.push_back(true);
+			}
+			else{
+				dirEntry->processorMask.push_back(false);
+			}
+		}
+		dirEntry->status = status;
+		directory[addr] = *(dirEntry);
+		return;
+	}
+	if(status == DirectoryEntryStatus.SHARED){
+		directory[addr].status = status;
+		directory[addr].processorMask[procID] = true;
+	}
+	if(status == DirectoryEntryStatus.MODIFIED){
+		directory[addr].status = status;
+		for(int i = 0; i < numContexts; i++){
+			if(i == procID){
+				directory[addr].processorMask[procID] = true;
+			}
+			else{
+				directory[addr].processorMask[procID] = false;
+			}
+		}
 
+	}
+	if(status == DirectoryEntryStatus.UNCACHED){
+		directory.erase(addr);
+
+	}
+}
+
+DirectoryEntry getOwners(uint64_t addr){
+	assert(directory.find(addr) != directory.end());
+	return directory[addr];
 
 }
 
--- backend/Simulator/directory.hpp
+++ backend/Simulator/directory.hpp
@@ -3,14 +3,13 @@
 
 #include "common.hpp"
 #include "context.hpp"
+#include <map>
 
 
 enum DirectoryEntryStatus {UNCACHED, SHARED, MODIFIED}  // by other processors
 
 struct DirectoryEntry
 {
-	// aligned address
-	unint64 addr;	
 	// a bit vector where bit i indicates whether Pi currently owns the line
 	std::vector<bool> processorMask;	
 	DirectoryEntryStatus status;
@@ -22,13 +21,15 @@ class Direcotry
 private:
 	Context* myContext;
 	int numContexts;
+	std::map<uint64_t addr, DirectoryEntry> directory;
 
 public:
 	Direcotry(Context* context);
 	~Direcotry();
 
-	LineStatus lookUpEntry(unint64 addr);
-	void addEntry(unint64 addr, LineStatus status);
+	DirectoryEntryStatus lookUpEntry(uint64_t addr);
+	void addEntry(uint64_t addr, DirectoryEntryStatus status, int procID);
+	DirectoryEntry getOwners(uint64_t addr);
 	void removeEntry(unint64 addr);
 };
 
